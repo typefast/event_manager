b705f1ed87db870a5ac7a1c9bfaedab1a394c0fd
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-04 20:18:44 +0000

    Add legislators_by_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,15 +7,27 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+  
+  legislator_names.join(", ")
+end
+
 puts "Event Manager Initialized!"
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
   
   zipcode = clean_zipcode(row[:zipcode])
   
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  legislators = legislators_by_zipcode(zipcode)
   
   puts "#{name} #{zipcode} #{legislators}"
 end
+
