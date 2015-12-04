479b9081461c02dc04fbd5e911d36be050926c9b
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-04 20:51:23 +0000

    add erb template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -8,18 +9,16 @@ def clean_zipcode(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-  
-  legislator_names.join(", ")
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
 puts "Event Manager Initialized!"
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 
 contents.each do |row|
   name = row[:first_name]
@@ -28,6 +27,7 @@ contents.each do |row|
   
   legislators = legislators_by_zipcode(zipcode)
   
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
 
