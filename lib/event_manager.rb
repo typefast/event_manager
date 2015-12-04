b4c3611dbe77e23b450fd1e5683b288a363a6882
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-04 21:11:20 +0000

    save_thank_you_letters method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -12,6 +12,16 @@ def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
+def save_thank_you_letters(id, form_letter)
+  DIR.mkdir("output") unless Dir.exists? "output"
+  
+  filename = "output/thanks_#{id}.html"
+  
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts "Event Manager Initialized!"
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
@@ -30,12 +40,6 @@ contents.each do |row|
   
   form_letter = erb_template.result(binding)
   
-  DIR.mkdir("output") unless Dir.exists? "output"
-  
-  filename = "output/thanks_#{id}.html"
-  
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
+  save_thank_you_letters(id, form_letter)
 end
 
